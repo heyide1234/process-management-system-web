ccaf22a132d7c16451219c8ad2f4ac0b05867630
HeZhi <he123zhi@163.com>
2026-05-18 09:08:02 +0800

    fixed:不同电脑访问

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,14 @@ import vue from '@vitejs/plugin-vue'
 export default defineConfig({
   plugins: [vue()],
   server: {
-    port: 5173
+    host: '0.0.0.0',
+    port: 5173,
+    proxy: {
+      '^/(api|engine-rest)': {
+        target: 'http://192.168.48.111:8080',
+        changeOrigin: true,
+        secure: false
+      }
+    }
   }
 })
